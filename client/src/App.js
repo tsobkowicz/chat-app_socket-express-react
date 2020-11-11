45713356b5a2baad190a283e79e9ece7301ffa96
Tom <tomaszsobkowicz21@gmail.com>
2020-11-11 13:40:19 +0100

    Setup server, create Join component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const App = () => <div>Hello world</div>;
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
+
+const App = () => (
+  <Router>
+    <Route path="/" exact component={Join} />
+    {/* not exact params becouse there will be qeuery parameteres in the route */}
+    <Route path="/chat" component={Chat} />
+  </Router>
+);
 
 export default App;
